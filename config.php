accb110323775ae029cad3672e45d48eb82bcf7d
Chao <30284733+chaoabunga@users.noreply.github.com>
2018-03-03 13:52:31 -0600

    Update config.php
    - api for live coin price and payout calculation
    - operator fee added

--- config.php
+++ config.php
@@ -32,33 +32,37 @@ SOFTWARE.
 $config = array(
     
     // e.g. Dogecoin
-    "coinname" => "Coinname",
+    "coinname" => "Chaincoin",
+    // coin and btc api's
+    "coin_api" => "https://www.cryptopia.co.nz/api/GetMarket/CHC_BTC", //for real time price and calculation of payout
+    "btc_api" => "https://www.bitstamp.net/api/ticker/",
+
     
 	// RPC settings:
 	// These are the settings you put into e.g. dogecoin.conf. They allow the faucet to interact with your wallet
-    "rpc_user" => "rpcuser",
-	"rpc_password" => "rpcpassword",
-	"rpc_host" => "rpchost",
-	"rpc_port" => "rpcport",
+    "rpc_user" => "",
+	"rpc_password" => "",
+	"rpc_host" => "",
+	"rpc_port" => "",
 
 	// MySQL settings:
-    "mysql_user" => "db_user",
-	"mysql_password" => "db_password",
+    "mysql_user" => "",
+	"mysql_password" => "",
 	"mysql_host" => "localhost",
-	"mysql_database" => "db_name", // faucet database name
+	"mysql_database" => "", // faucet database name
 	"mysql_table_prefix" => "sf_", // table prefix to use
 
 	// Coin values:
-	"minimum_payout" => 1, // minimum coins to be awarded
-	"maximum_payout" => 6, // maximum coins to be awarded
-	"payout_threshold" => 10, // payout threshold, if the faucet contains less coins than this, display the 'dry_faucet' message
-	"payout_interval" => "1h", // payout interval, the wait time for a user between payouts. Type any numerical value with either a "m" (minutes), "h" (hours), or "d" (days), attached. Examples: 50m for a 50 minute delay, 7h for a 7 hour delay, etc.
+	"minimum_payout" => 0.03, // minimum coins to be awarded
+	"maximum_payout" => 0.03, // maximum coins to be awarded in $ value
+	"payout_threshold" => 1, // payout threshold, if the faucet contains less coins than this, display the 'dry_faucet' message
+	"payout_interval" => "3h", // payout interval, the wait time for a user between payouts. Type any numerical value with either a "m" (minutes), "h" (hours), or "d" (days), attached. Examples: 50m for a 50 minute delay, 7h for a 7 hour delay, etc.
 
     
     // Payment system:
 	"stage_payments" => true, // stage payments in the database, to be executed later
-	"stage_payment_account_name" => "", // account name to send transactions with, needs to be valid // you also can leave it empty
-	"staged_payment_threshold" => 25, // staged payment threshold, all staged payments are executed when this number is reached
+	"stage_payment_account_name" => "faucet", // account name to send transactions with, needs to be valid // you also can leave it empty
+	"staged_payment_threshold" => 100, // staged payment threshold, all staged payments are executed when this number is reached
 	"staged_payment_cron_only" => false, // ignore the stage_amount counter, only execute staged payments when the cron script is called
     
 	// this option has 3 possible values: "ip_address", "coin_address", and "both". It defines what to check for when a user enters a coin address in order to decide whether or not to award coins to this user.
@@ -69,14 +73,14 @@ $config = array(
 
 	"use_captcha" => true, // require the user to enter a captcha
 
-	"captcha" => "recaptcha", // valid options: recaptcha, recaptcha2
+	"captcha" => "recaptcha2", // valid options: recaptcha, recaptcha2
 
-	"captcha_https" => false, // use https (only for recaptcha2) valid options: true, false
+	"captcha_https" => true, // use https (only for recaptcha2) valid options: true, false
 
 	// enter your private and public reCAPTCHA key here:
 	"captcha_config" => array(
-		"private_key" => "privatekey",
-		"public_key" => "publickey"
+		"private_key" => "",
+		"public_key" => ""
 		),
 
     
@@ -87,18 +91,21 @@ $config = array(
     // promo codes:
 	"use_promo_codes" => true, // accept promo codes
 
+	"operator_fee" => 5, //% fee for faucet operator,
+	"operator_address" => "",
+
     
 	// if the wallet is encrypted, enter the PASSPHRASE here. Leave it blank otherwise!
 	"wallet_passphrase" => "",
 
 	// Donation address:
-	"donation_address" => "DTiUqjQTXwgZfvcTcdoabp7uLezK47TPkN", // donation address to display
+	"donation_address" => "", // donation address to display
 
 	// Faucet look and feel:
-	"title" => "Cryptocurrency faucet script", // page title, may be used by the template too
+	"title" => "Faucet", // page title, may be used by the template too
 	"template" => "default", // template to use (see the templates directory)
     //code for advertisements:
-    "ads" => "<iframe></iframe>"
+	"ads" => ""
 	);
 
 
