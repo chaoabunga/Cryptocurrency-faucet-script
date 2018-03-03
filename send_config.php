5ead0070ff688918d468286ddc8c0310e95c9705
Chao <30284733+chaoabunga@users.noreply.github.com>
2018-03-03 13:50:06 -0600

    Update send_config.php
    -exchange apis and operator varables added.

--- send_config.php
+++ send_config.php
@@ -6,7 +6,6 @@
 The MIT License (MIT)
 
 Copyright (c) [2014] [Christian Grieger]
-Copyright (c) [2018] [Chao Han]
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
@@ -31,37 +30,41 @@ SOFTWARE.
 // Modify these settings to suit your needs.
 
 $config = array(
-
+    
     // e.g. Dogecoin
     "coinname" => "Send",
+    // coin and btc api's
+    "coin_api" => "https://www.cryptopia.co.nz/api/GetMarket/SEND_BTC",
+    "btc_api" => "https://www.bitstamp.net/api/ticker/",
 
+    
 	// RPC settings:
 	// These are the settings you put into e.g. dogecoin.conf. They allow the faucet to interact with your wallet
-    "rpc_user" => "senduser",
-	"rpc_password" => "xxxx",
-	"rpc_host" => "localhost",
-	"rpc_port" => "50051",
+    "rpc_user" => "",
+	"rpc_password" => "",
+	"rpc_host" => "",
+	"rpc_port" => "",
 
 	// MySQL settings:
-    "mysql_user" => "faucetuser",
-	"mysql_password" => "xxxx",
+    "mysql_user" => "",
+	"mysql_password" => "",
 	"mysql_host" => "localhost",
-	"mysql_database" => "faucet", // faucet database name
+	"mysql_database" => "", // faucet database name
 	"mysql_table_prefix" => "sf_", // table prefix to use
 
 	// Coin values:
-	"minimum_payout" => 0.1, // minimum coins to be awarded
-	"maximum_payout" => 0.1, // maximum coins to be awarded
+	"minimum_payout" => 0.03, // minimum coins to be awarded
+	"maximum_payout" => 0.03, // maximum coins to be awarded in $ value
 	"payout_threshold" => 1, // payout threshold, if the faucet contains less coins than this, display the 'dry_faucet' message
-	"payout_interval" => "2h", // payout interval, the wait time for a user between payouts. Type any numerical value with either a "m" (minutes), "h" (hours), or "d" (days), attached. Examples: 50m for a 50 minute delay, 7h for a 7 hour delay, etc.
+	"payout_interval" => "3h", // payout interval, the wait time for a user between payouts. Type any numerical value with either a "m" (minutes), "h" (hours), or "d" (days), attached. Examples: 50m for a 50 minute delay, 7h for a 7 hour delay, etc.
 
-
+    
     // Payment system:
 	"stage_payments" => true, // stage payments in the database, to be executed later
 	"stage_payment_account_name" => "faucet", // account name to send transactions with, needs to be valid // you also can leave it empty
 	"staged_payment_threshold" => 100, // staged payment threshold, all staged payments are executed when this number is reached
 	"staged_payment_cron_only" => false, // ignore the stage_amount counter, only execute staged payments when the cron script is called
-
+    
 	// this option has 3 possible values: "ip_address", "coin_address", and "both". It defines what to check for when a user enters a coin address in order to decide whether or not to award coins to this user.
 	// "ip_address": checks the user IP address in the payout history.
 	// "coin_address": checks the user coins address in the payout history.
@@ -80,32 +83,29 @@ $config = array(
 		"public_key" => ""
 		),
 
-
+    
     // proxy filter:
 	"filter_proxies" => true, // whether to filter proxies or not. It's up to you to fill the proxy ban table. (see also the tor node cron job in ./lib/proxy_filter/cron/tor.php)
 	"proxy_filter_use_faucet_database" => false, // whether the proxy filter should use the faucet database connection or not. (if set to false, the proxy filter will connect to the database set in ./lib/proxy_filter/config.php)
-
+    
     // promo codes:
 	"use_promo_codes" => true, // accept promo codes
 
+	"operator_fee" => 5, //% fee for faucet operator,
+	"operator_address" => "",
 
+    
 	// if the wallet is encrypted, enter the PASSPHRASE here. Leave it blank otherwise!
 	"wallet_passphrase" => "",
 
 	// Donation address:
-	"donation_address" => "ShNirqG6JN3FNf8di7e9TXoo2wdiyRy3sV", // donation address to display
+	"donation_address" => "", // donation address to display
 
 	// Faucet look and feel:
 	"title" => "SocialSend Faucet", // page title, may be used by the template too
 	"template" => "default", // template to use (see the templates directory)
     //code for advertisements:
-	"ads" => "<b>Referrrals:</b><br> <b>
-	<a href='https://www.coinbase.com/join/5301c2ad74a395a7370000ec'>Coinbase</a>
-	| <a href='https://www.binance.com/?ref=12632017'>Binance</a>
-	| <a href='https://www.cryptopia.co.nz/Register?referrer=chaoabunga'>Cryptopia</a>
-	| <a href='https://www.coinexchange.io/?r=45728381'>CoinExchange</a>
-	| <a href='https://wallet.crypto-bridge.org/?r=chaoabunga'>Crypto-Bridge</a>
-	<br> *<a href='https://socialsend.io/'>Social Send Wallet Download</a></b>*"
+	"ads" => ""
 	);
 
 
